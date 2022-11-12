c461c29a7d40c4d88c1c8b2cfbf331d8d2641324
Ashutosh Sharma <ashutoshbhardwaj060@gmail.com>
2022-11-12 18:29:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import "./Components/custom.css";
+import './App.css';
+import Nav from './Components/Nav';
+import Back from "./Components/Back";
+import { Routes, Route } from "react-router-dom";
+import Info from './Components/Info';
+import About from './Components/About';
+import Services from "./Components/Services";
+import Contact from "./Components/Contact";
+import Whychoose from "./Components/Whychoose";
+import Login from "./Components/Login";
+import Registration from "./Components/Registration";
+import Userpage from "./Components/Userpage.js";
+import Userpagenew from "./Components/Userpagenew";
+
+
+function App() {
+ 
+  
+  return (
+    <body>
+      
+        <Routes>
+          <Route path="login" element={<Login />} />
+          <Route path="nav" element={<Nav/>}/>
+          <Route path="registration" element={<Registration/>}/>
+          <Route path="about" element={<About/>}/>
+          <Route path="services" element={<Services/>}/>
+          <Route path="whychoose" element={<Whychoose/>}/>
+          <Route path="contact" element={<Contact/>}/>
+          <Route path="userpagenew" element={<Userpagenew/>}/>
+          <Route exact path="userpage" element={<Userpage/>}/>
+         
+          
+        </Routes>
+    
+        <div>
+        <header id="header" className="fixed-top d-flex align-items-center">
+          <div className="container">
+            <Nav />
+      
+
+          </div>
+        </header>
+        <Back />
+      </div>
+      <div>
+        <Info />
+
+
+      </div>
+      <main id="main">
+        <section>
+          <About />
+        </section>
+        <section>
+          <Services />
+        </section>
+        <Whychoose />
+
+        <Contact />
+        
+      </main>
+ 
+    </body >
+
+  );
+}
+
+export default App;
---
+++ src/data1.js
@@ -0,0 +1,34 @@
+const list2=[
+    {
+        id:1,
+        img:"https://m.media-amazon.com/images/I/71Iq9ug6OvL._SX522_.jpg",
+        title: " Redmi Note 11 (Starburst White, 4GB RAM, 64GB Storage) | 90Hz FHD+ AMOLED Display | Qualcomm® Snapdragon™ 680-6nm  ",
+        price: 12999
+
+    },
+    {
+        id:2,
+        img:"https://m.media-amazon.com/images/I/41USxvs+NlL._AC_SY780_FMwebp_.jpg",
+        title: " Apple iPhone 11 (128GB) - Yellow   ",
+        price: 49900
+
+    },
+    {
+        id:3,
+        img:"https://m.media-amazon.com/images/I/41fVP+e7CzS._AC_SY580_FMwebp_.jpg",
+        title: "    OnePlus 80 cm (32 inches) Y Series HD Ready LED Smart Android TV 32Y1 (Black)             ",
+        price: 25000
+
+    },
+    {
+        id:4,
+        img:"https://images-eu.ssl-images-amazon.com/images/I/41ohhMUbcuL._SX300_SY300_QL70_FMwebp_.jpg",
+        title: "Amazon Brand - Solimo Lunaria Fabric 4 Seater L Shape Sofa (Grey & Blue)",
+        price: 17499
+
+    }
+];
+
+
+
+export default list2;
